78825cbfd060aa6ca98967e662bbd82c0e1af106
zamparg <zamparg@hotmail.com>
2023-02-28 12:43:13 -0300

    task_2: cards dinamicas

---
+++ scripts/upcoming-events.js
@@ -0,0 +1,22 @@
+let dataUpcoming = data.events.filter(event => event.date >= data.currentDate)
+
+dataUpcoming.forEach(element => {
+    document.getElementById("cardsUpcoming").innerHTML+=`
+    <div class="col">
+                <div class="card h-100">
+                    <img src="${element.image}" class="card-img-top p-2" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.name}</h5>
+                        <p class="card-text">${element.description}</p>
+                    </div>
+                    <div class="card-footer d-flex justify-content-around align-items-center">
+                        <small> $ ${element.price} </small>
+                        <a href="./pages/details.html" class="btn btn-outline-success search-btn p-0">
+                            View more
+                        </a>
+                    </div>
+                </div>
+            </div>
+    
+    `;
+})
